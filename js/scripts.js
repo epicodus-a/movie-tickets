5c8efef38d836ccf9cb3445bfb840f7736de736d
Adriana C and julia sheremet <julia@>
2018-04-03 14:55:37 -0700

    fix errors in js

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,14 @@
 //business logic
 
 // movieName='m', price = 10, age="teen", time=0
-function Movie(movieName, price, age, time) { //constructor
+function Ticket(movieName, price, age, time) { //constructor
   this.movieName = movieName;
   this.price = price;
   this.age = age;
   this.time = time;
 }
 
-Movie.prototype.calculatePrice = function() {// this method belongs to the constructor so you dont need to pass the perameters through this method/function
+Ticket.prototype.calculatePrice = function() {// this method belongs to the constructor so you dont need to pass the perameters through this method/function
   if (this.age === "13-17") {
     this.price -= 2;
   } else if (this.age === "55+") {
@@ -19,6 +19,13 @@ Movie.prototype.calculatePrice = function() {// this method belongs to the const
     this.price +=2;
   }
   return this.price;
+}
+
+function resetFields() {
+  $("#movie").val("");
+  $("input:radio[name=time]:checked").val("");
+  $("#age").val("");
+  $("#date").val("");
 }
 
 // Movie.prototype.calculateTimePrice = function() {
@@ -43,19 +50,19 @@ $(document).ready(function(){
   var movieAge = $("#age").val();
   var movieDate = $("#date").val();
 
-  var newMovie = new Movie(movieChoice, 10, movieAge, movieTime);
+  var newTicket = new Ticket(movieChoice, 10, movieAge, movieTime);
 
   // $("#user-output").show();
-  var agePrice = newMovie.calculatePrice();
+  var agePrice = newTicket.calculatePrice();
   var output = `<p class='lead outputDisplay'> Movie Choice: ${movieChoice}</p>
                 <p class='lead outputDisplay'> Movie Time: ${movieTime}</p>
                 <p class='lead outputDisplay'> Your Age: ${movieAge}</p>
                 <p class='lead outputDisplay'> Movie Date: ${movieDate}</p>
                 <p class='lead outputDisplay'> Movie Price: $ ${agePrice}</p>
                 `;
-  $("#user-output").append(output);
+  $("#user-output").html(output);// adds to the page and replaces the original - use this instead of append
   $("#user-output").fadeIn("slow"); //dont need a .show if you're using fadeIn
   // $("#user-output").append("$" + agePrice);
-
+  resetFields();
   });
 });
